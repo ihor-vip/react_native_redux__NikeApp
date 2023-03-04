a6ed7ddf7907013f1eeefef4acf368d9d75ce878
ihor-vip <vip20140902@gmail.com>
2023-03-04 12:40:32 +0200

    display list of products in 2 columns

--- App.js
+++ App.js
@@ -1,31 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View, Image, FlatList} from 'react-native';
 import products from './src/data/products'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <FlatList
-          data={products}
-          renderItem={({item}) => (
-              <Image source={{uri: item.image}} style={styles.image} />
-          )}
-      />
+    return (
+        <View style={styles.container}>
+            <FlatList
+                data={products}
+                renderItem={({item}) => (
+                    <View style={styles.itemContainer}>
+                        <Image source={{uri: item.image}} style={styles.image} />
+                    </View>
+                )}
+                numColumns={2}
+            />
 
-      <StatusBar style="auto" />
-    </View>
-  );
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  image: {
-    width: "100%",
-    aspectRatio: 1
-  }
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    itemContainer: {
+        width: "50%",
+        padding: 1,
+    },
+    image: {
+        width: "100%",
+        aspectRatio: 1,
+    },
 });
